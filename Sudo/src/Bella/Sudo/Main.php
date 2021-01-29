1bda331c8ed082f64ab248971b422f81c7b622af
StrawBerrez <bellakittyylover@gmail.com>
2021-01-29 00:39:06 -0500

    Defined $cmd

--- Sudo/src/Bella/Sudo/Main.php
+++ Sudo/src/Bella/Sudo/Main.php
@@ -13,7 +13,7 @@ class Main extends PluginBase{
     public function onEnable(){
     }
 
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool{
 
         switch($cmd->getName()){
             case "sudo":
