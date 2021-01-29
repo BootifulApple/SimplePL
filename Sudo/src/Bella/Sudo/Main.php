ce0aee315b88b21a9d79f57d709d805dc81c845c
StrawBerrez <bellakittyylover@gmail.com>
2021-01-29 01:06:06 -0500

    fixed bugs

--- Sudo/src/Bella/Sudo/Main.php
+++ Sudo/src/Bella/Sudo/Main.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace Bella\Sudo;
 
+use jojoe77777\FormAPI\Form;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Player;
-use pocketmine\command\Command, pocketmine\command\CommandSender;
-use pocketmine\evet\Listener;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
 
-class Main extends PluginBase{
+class Main extends PluginBase implements Listener{
     public function onEnable(){
     }
 
@@ -18,17 +20,8 @@ class Main extends PluginBase{
         switch($cmd->getName()){
             case "sudo":
                 if($sender instanceof Player){
-                    if($data === null){
-                        return true;
-                    }
-                    $p = $this->getServer()->getPlayer($data[0])->getName();
-                    $cmd = $data[1];
-                    if($p instanceof Player){
-                        $this->getServer()->dispatchCommand($p, $cmd);
-                        $sender->sendMessage("You Sudoed " . $data[0 . " todo /" . $data[1]]);
                         if($sender->hasPermission("sudo.perm")) {
                             $this->sudo($sender);
-                    }
 
                     }else{
                         $sender->sendMessage("You do have permission to use this command.");
@@ -36,5 +29,24 @@ class Main extends PluginBase{
                 }
         }
         return true;
+    }
+
+    public function sudo($player){
+        $form = $this->getServer()->getPluginManager()->getPlugin("FormAPI")->createCustomForm(function (Player $player, array $data = null) {
+            if($data === null) {
+                return true;
+            }
+            $p = $this->getServer()->getPlayer($data[0]);
+            $cmd = $data[1];
+            if ($p instanceof Player) {
+                $this->getServer()->dispatchCommand($p, $cmd);
+                $player->sendMessage("You Sudoed " . $p->getName() . " todo /" . $data[1]);
+            }
+        });
+        $form->setTitle("SudoUI");
+        $form->addInput("Type a player's name");
+        $form->addInput("Type a command without the /");
+        $form->sendToPlayer($player);
+        return $form;
     }
 }
